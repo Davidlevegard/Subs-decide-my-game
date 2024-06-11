1758e2caaabe7b8e667750d62631e7b08a785895
Davidlevegard <david.levegard@elev.ga.lbs.se>
2024-06-11 22:11:36 +0200

    Initial commit

---
+++ Assets/scripts/GridPlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    public LayerMask whatStopsMovement;
+
+    public Animator anim;
+
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves player to move point
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        //checks if move point can be moves
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            //checks if horizontal input has been pressed
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+
+                //checks if collision in the way
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                {
+                    //moves move point in horizontal axis
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+
+            }
+
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+                {
+                    //moves move point in vertical axis
+
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+
+            }
+
+            anim.SetBool("moving", false);
+
+        }
+        else
+        {
+            anim.SetBool("moving", true);
+        }
+
+    }
+}
